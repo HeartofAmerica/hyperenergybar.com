212e1ccc9283d547c751132ffb4b606dea9193d2
Heart of America <tcain@hoari.com>
2024-11-01 12:14:24 -0500

    update redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/coffee',
+          destination: '/menu/coffee',
+          permanent: false,
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
